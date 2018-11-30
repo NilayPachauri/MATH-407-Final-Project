52efa0aaffea9111d5f6fb9925247c4ca253187b
Nilay Pachauri <pachauri@usc.edu>
2018-11-30 04:38:52 -0800

    Found Area of Antarctica, need to generate images

--- random_checker.py
+++ random_checker.py
@@ -1,6 +1,26 @@
-def principal_period(s):
-    i = (s+s).find(s, 1, -1)
-    return None if i == -1 else s[:i]
+import sys
 
-if __name__ = '__main__':
-	print(principal_period('0045662100456621004566210045662100456621'))
+def principal_period(s):
+	i = (s+s).find(s, 1, -1)
+	return None if i == -1 else s[:i]
+
+def convert_to_binary(num):
+	return "{0:#b}".format(num)
+
+def compute_sum_of_all_numbers_string():
+	a = ""
+	for x in range (0, 10):
+		a += convert_to_binary(x)
+
+	print(a)
+
+if __name__ == '__main__':
+
+	compute_sum_of_all_numbers_string()
+
+	x = principal_period('004608294930875576036866359447')
+	if x == None:
+		print("This is a unique random number");
+	else:
+		print("This number repeats with the following string: " + x);
+
--- sphere.py
+++ sphere.py
@@ -8,6 +8,8 @@ def sample_spherical(npoints, ndim=3):
     vec /= np.linalg.norm(vec, axis=0)
     return vec
 
+def antarctica_bound(num):
+	return num < -(82.8628/90) and num > -1
 
 if __name__ == '__main__':
 	phi = np.linspace(0, np.pi, 20)
@@ -20,5 +22,16 @@ if __name__ == '__main__':
 
 	fig, ax = plt.subplots(1, 1, subplot_kw={'projection':'3d', 'aspect':'equal'})
 	ax.plot_wireframe(x, y, z, color='k', rstride=1, cstride=1)
+	ax.set_xlabel('X axis')
+	ax.set_ylabel('Y axis')
+	ax.set_zlabel('Z axis')
+	ax.text2D(0.05, 0.95, "10 Points", transform=ax.transAxes)
+
+	print(len(zi))
+
+	total_zi = [x for x in zi if antarctica_bound(x)]
+
+	print(len(total_zi))
+
 	ax.scatter(xi, yi, zi, s=10, c='r', zorder=10)
 	plt.show()
